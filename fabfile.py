5ebd74b4cc2927c8e3b3166ef028a8803c8b8d8b
Pavka <pavkazzz@mail.ru>
2014-11-28 16:54:47 +0300

    fabric

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,13 @@
-from fabric.api import local, env
-
+from fabric.api import env, sudo
+env.hosts = ['178.62.208.38/']
 env.user = 'pavka'
 env.password = 'ppppoi'
 code_dir = '/home/pavka'
 
 
 def update_site():
-    #sudo('git pull')
-    local('echo "ppppoi\n" |sudo git pull')
-    local('echo "ppppoi\n" |supervisorctl restart flask - blog')
+    sudo('git pull')
+    sudo('supervisorctl restart flask - blog')
 
 if __name__ == '__main__':
     update_site()
